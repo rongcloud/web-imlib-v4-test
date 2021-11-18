834f9674a17da74f74bbc77c92f55e51db8ea5f1
lizhipeng <lizhipeng@rongcloud.cn>
2021-11-18 15:15:12 +0800

    feat: web imlib v4 test

---
+++ config.js
@@ -0,0 +1,33 @@
+(function (win) {
+
+  var isDebug = false;
+
+  var im = {
+    appkey: '',
+    token: '',
+    navi: '',
+    targetId: '',
+    customCMP: '',
+    isPolling: false,
+  };
+
+  if (!isDebug) {
+    delete im.cmpUrl;
+  }
+
+  var config = {
+    im: im,
+    isDebug: isDebug,
+    debugConf: {
+      autoRun: false,
+      isShowMsg: false
+    }
+  };
+
+  win.RongIM = win.RongIM || {};
+  win.RongIM = {
+    config: config,
+    components: {}
+  };
+
+})(window);
